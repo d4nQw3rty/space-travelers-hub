625ae0937cd2fa7a71557f678fcf92c4d9acacae
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-11-14 16:38:07 -0600

    Add NavBar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,47 @@
 import { NavLink } from 'react-router-dom';
+import s from './NavBar.module.css';
 
 const img = require('../assets/planet.png');
 
+const handleActive = ({ isActive }) => (isActive
+  ? {
+    color: 'blue',
+    textDecoration: 'underline',
+  }
+  : { });
+
 const NavBar = () => (
-  <header className="top-section d-flex">
-    <NavLink to="/" className="logo">
+  <header className={s.headerContainer}>
+    <NavLink to="/" className={s.logo}>
       <img src={img} alt="Logo" />
       <h1>
         Space Travelers&apos; Hub
       </h1>
     </NavLink>
-    <nav className="d-flex">
-      <NavLink to="/">
+    <nav className={s.navigation}>
+      <NavLink
+        to="/"
+        style={handleActive}
+      >
         Rockets
       </NavLink>
-      <NavLink to="missions">
+      <NavLink
+        to="missions"
+        style={handleActive}
+      >
         Missions
       </NavLink>
-      <NavLink to="dragons">
+      <NavLink
+        to="dragons"
+        style={handleActive}
+      >
         Dragons
       </NavLink>
-      <NavLink to="myprofile">
+      <span> | </span>
+      <NavLink
+        to="myprofile"
+        style={handleActive}
+      >
         My Profile
       </NavLink>
     </nav>
