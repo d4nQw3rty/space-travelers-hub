6bb4b497c307aa890605e245e8c538b03ff87b4e
d4nQw3rty <danlinmoy@gmail.com>
2022-11-15 13:32:51 -0400

    Update Missions.js

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import style from './Missions.module.css';
+import { fetchMissions } from '../redux/reducer/missions/missionsSlice';
+// import missionsReducer from '../missions/missionsSlice';
 
-const Missions = () => (
-  <div>
-    <div className={style.container}>
-      <div className={style.itemContainer}>
-        <p className={`${style.itemClass1} ${style.itemPadding}`}>Mission</p>
-        <p className={`${style.itemClass2} ${style.itemPadding}`}>Description</p>
-        <p className={`${style.itemClass1} ${style.itemPadding}`}>Status</p>
-        <p className={`${style.itemClass1} ${style.itemPadding}`} />
-      </div>
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+  const status = useSelector((state) => state.missions.status);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchMissions());
+    }
+  }, [status, dispatch]);
+
+  const renderMissions = () => {
+    if (status === 'loading') {
+      return <div>Loading...</div>;
+    }
+    if (status === 'succeeded') {
+      return missions.map((mission) => (
+        <div key={mission.mission_id} className={style.mission}>
+          <h3>{mission.mission_name}</h3>
+          <p>{mission.description}</p>
+        </div>
+      ));
+    }
+    if (status === 'failed') {
+      return <div>Failed to load missions</div>;
+    }
+  };
+
+  return (
+    <div className={style.missionsContainer}>
+      <h2>Missions</h2>
+      {renderMissions()}
     </div>
-  </div>
-);
+  );
+}
 
 export default Missions;
