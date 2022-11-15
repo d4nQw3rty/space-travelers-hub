674ade994467a67632d60508f6f0267acdc400df
d4nQw3rty <danlinmoy@gmail.com>
2022-11-15 11:36:50 -0400

    Create configureStore.js

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// imprt dragonsReducer from '../dragons/dragonsSlice';
+// import missionsReducer from '../missions/missionsSlice';
+// import rocketsReducer from '../rockets/rocketsSlice';
+
+/*export const store = configureStore({
+  reducer: {
+    Dragons: dragonsReducer,
+    Missions: missionsReducer,
+    Rockets: rocketsReducer,
+  },
+}); */
