72cbef3ea3fcd9b4e2c24b654306b504b7d72b81
d4nQw3rty <danlinmoy@gmail.com>
2022-11-15 15:46:08 -0400

    fix linter

--- src/index.js
+++ src/index.js
@@ -9,11 +9,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 );
