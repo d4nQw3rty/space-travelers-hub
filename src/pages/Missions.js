aa6afa93b5f3154586cdecb6b11f1d95f860e99e
d4nQw3rty <danlinmoy@gmail.com>
2022-11-15 11:36:04 -0400

    Create Mission container

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,5 +1,15 @@
+import style from './Missions.module.css';
 const Missions = () => (
-  <div>Missions</div>
+  <div>
+      <table className={style.thead}>
+        <tr>
+          <td className= {style.td1}>Mission</td>
+          <td className= {style.td2}>Description</td>
+          <td>Status</td>
+          <td></td>
+        </tr>        
+      </table>
+    </div>
 );
 
 export default Missions;
