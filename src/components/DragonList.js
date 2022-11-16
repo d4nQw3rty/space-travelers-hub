0f49535681a4025f78d9eb6dc71f01ada3da8b85
Solly <solobarine@gmail.com>
2022-11-16 07:23:39 +0100

    Refactor dragon reducer from ducks to slices/Create Dragon component for UI

---
+++ src/components/DragonList.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import fetchDragons from '../redux/reducer/dragons/fetchDragons';
+import Dragon from './Dragons';
+
+const DragonList = () => {
+  const dragons = useSelector((state) => state.dragons.dragons);
+  const status = useSelector((state) => state.dragons.result);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDragons());
+  }, [status, dispatch]);
+
+  console.log(dragons);
+
+  const showDragons = () => {
+    if (status === 'loading') return <div className="loading"><p className="message">Loading. Please Wait.</p></div>;
+    if (status === 'failed') return <div className="fail"><p className="failMessage">Sorry, the Dragons are not avaliable right now. Try again later</p></div>;
+    return dragons.map((dragon) => { // eslint-disable-line
+      <Dragon props={dragon} />;
+    });
+  };
+
+  return (
+    <div className="dragonList">
+      <h1 className="list">List of Dragons</h1>
+      {showDragons()}
+    </div>
+  );
+};
+
+export default DragonList;
