5141192ef8bd1ff2efebd9e24cece50b25cede68
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-11-17 13:50:46 -0600

    Add rockets to my profile page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -4,6 +4,7 @@ import style from './MyProfile.module.css';
 
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions.missions);
+  const rockets = useSelector((state) => state.rockets.rockets);
   return (
     <div className={style.MyProfile}>
       <div className={style.MyProfileChild}>
@@ -15,6 +16,17 @@ const MyProfile = () => {
             ),
           )}
         </ul>
+      </div>
+      <div className={style.MyProfileChild}>
+        <h2 className={style.ChildTitle}>My Rockets</h2>
+        <ul className={style.list}>
+          {rockets.map(
+            (rockets) => rockets.reserved && (
+            <li key={rockets.id} className={style.listChild}>{rockets.rocket_name}</li>
+            ),
+          )}
+        </ul>
+
       </div>
     </div>
   );
