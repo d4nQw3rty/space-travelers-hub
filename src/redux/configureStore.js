55e611c7e954c18d8b87acb4054e9f79499f5c3d
d4nQw3rty <danlinmoy@gmail.com>
2022-11-17 11:51:43 -0400

    Fix linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-// imprt dragonsReducer from '../dragons/dragonsSlice';
 import missionsReducer from './reducer/missions/missionsSlice';
-// import rocketsReducer from '../rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
