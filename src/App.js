4ad962ecf48f57d33b2f959d937bbb8991fded9f
Akpuru Solomon Barine <solobarine@gmail.com>
2022-11-18 16:33:14 +0100

    remove file extension for fetchDragons

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import MyProfile from './pages/MyProfile';
 import NavBar from './components/NavBar';
 import { fetchRockets } from './redux/reducer/rockets/rocketsSlice';
 import { fetchMissions } from './redux/reducer/missions/missionsSlice';
-import fetchDragons from './redux/reducer/dragons/fetchDragon.js';
+import fetchDragons from './redux/reducer/dragons/fetchDragon'; // eslint-disable-line
 
 const App = () => {
   const dispatch = useDispatch();
