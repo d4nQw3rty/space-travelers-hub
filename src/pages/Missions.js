94aa8c17d46f34496a5072cf6e7d842d2a553ecc
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-11-18 14:19:53 -0600

    Add margin to missions page

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -12,50 +12,52 @@ const Missions = () => {
 
   return (
     <>
-      <div>
-        <div className={style.container}>
-          <div className={style.itemContainer}>
-            <p className={`${style.itemClass1} ${style.itemPadding}`}>
-              Mission
-            </p>
-            <p className={`${style.itemClass2} ${style.itemPadding}`}>
-              Description
-            </p>
-            <p className={`${style.itemClass1} ${style.itemPadding}`}>Status</p>
-            <p className={`${style.itemClass1} ${style.itemPadding}`} />
-          </div>
-        </div>
-      </div>
-      {missions.map((mission, index) => (
-        <div key={mission.id} id={index + 1} className={style.item}>
+      <div className={style.bigBox}>
+        <div>
           <div className={style.container}>
             <div className={style.itemContainer}>
               <p className={`${style.itemClass1} ${style.itemPadding}`}>
-                {mission.mission_name}
+                Mission
               </p>
               <p className={`${style.itemClass2} ${style.itemPadding}`}>
-                {mission.description}
+                Description
               </p>
-              <p className={`${style.itemClass1} ${style.itemPadding} ${style.badge}`}>
-                {
+              <p className={`${style.itemClass1} ${style.itemPadding}`}>Status</p>
+              <p className={`${style.itemClass1} ${style.itemPadding}`} />
+            </div>
+          </div>
+        </div>
+        {missions.map((mission, index) => (
+          <div key={mission.id} id={index + 1} className={style.item}>
+            <div className={style.container}>
+              <div className={style.itemContainer}>
+                <p className={`${style.itemClass1} ${style.itemPadding}`}>
+                  {mission.mission_name}
+                </p>
+                <p className={`${style.itemClass2} ${style.itemPadding}`}>
+                  {mission.description}
+                </p>
+                <p className={`${style.itemClass1} ${style.itemPadding} ${style.badge}`}>
+                  {
                   mission.reserved
                     ? (<span className={style.active}>Active Member</span>)
                     : (<span className={style.inactive}>NOT A MEMBER</span>)
                 }
-              </p>
-              <div
-                className={`${style.itemClass1} ${style.itemPadding} ${style.button}`}
-              >
-                {
+                </p>
+                <div
+                  className={`${style.itemClass1} ${style.itemPadding} ${style.button}`}
+                >
+                  {
                   mission.reserved
                     ? (<button type="button" className={style.buttonLeaveMission} onClick={() => dispatch(leaveMission(mission))}>Leave Mission</button>)
                     : (<button type="button" className={style.buttonJoinMission} onClick={() => dispatch(joinMission(mission))}>Join Mission</button>)
                 }
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   );
 };
